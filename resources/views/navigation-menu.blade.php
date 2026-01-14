40800c8e6f7f08965849234afc3f2fb7e9386af3
1885979-ArianaDAW2 <1885979@alu.murciaeduca.es>
2026-01-14 18:51:23 +0100

    9.5.7 probando el @can

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,13 +15,15 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link href="{{ route('services.index') }}" :active="request()->routeIs('services.index')">
+                    <x-nav-link href="{{ route('services') }}" :active="request()->routeIs('services.index')">
                         {{ __('Servicios') }}
                     </x-nav-link>
-                    <x-nav-link href="{{ route('control-panel') }}"
-                                :active="request()->routeIs('services.index')">
-                        {{ __('Servicios') }}
-                    </x-nav-link>
+                    @can('manage_users')
+                        <x-nav-link href="{{ route('admin.control-panel') }}"
+                                    :active="request()->routeIs('admin.control-panel')">
+                            {{ __('Panel de control') }}
+                        </x-nav-link>
+                    @endcan
                 </div>
             </div>
 
@@ -36,7 +38,7 @@
                                             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                         {{ Auth::user()->currentTeam->name }}
 
-                                        <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                        <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg"
                                              viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                   d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"/>
@@ -97,7 +99,7 @@
                                             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                         {{ Auth::user()->name }}
 
-                                        <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                        <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg"
                                              viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                   d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
@@ -143,7 +145,7 @@
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open"
                         class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="size-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <svg class="size-6" stroke="currentColor" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
                               stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                               d="M4 6h16M4 12h16M4 18h16"/>
