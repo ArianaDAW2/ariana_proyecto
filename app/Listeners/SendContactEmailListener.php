ae5100c98b54e199d2db05ed385991e615bf46b1
1885979-ArianaDAW2 <1885979@alu.murciaeduca.es>
2026-01-25 21:16:15 +0100

    0.5.13.5 1 smtp configurado

--- app/Listeners/SendContactEmailListener.php
+++ app/Listeners/SendContactEmailListener.php
@@ -10,7 +10,7 @@ class SendContactEmailListener
 {
     public function handle(ContactFormSubmitEvent $event): void
     {
-        Mail::to('1885979@alu.murciaeduca.es')->send(new ContactMail(
+        Mail::to('ariana.irissa@gmail.com')->send(new ContactMail(
             $event->title,
             $event->email,
             $event->message
